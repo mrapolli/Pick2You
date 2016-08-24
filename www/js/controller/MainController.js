98102cc4805dfdbd88ee3a8b76e7fe353f9f35be
Abdon Megale Neto <amegale@stefanini.com>
2016-08-24 18:40:24 -0300

    Refactoring

---
+++ www/js/controller/MainController.js
@@ -0,0 +1,96 @@
+angular.module('starter').controller('MainController', ['$scope', '$rootScope',  '$location' ,'$q', '$ionicLoading', 'Scopes', function($scope,  $rootScope , $location ,  $q, $ionicLoading, Scopes) {
+  Scopes.store('MainController', $scope);
+
+  $scope.vizualiza = [{"valor" : ''}]
+  $scope.fotos = [];
+  $scope.shouldShowDelete = false;
+  $scope.shouldShowDeleteImage = false;
+
+  $scope.habilitaDeleteImagem = function(){
+    $scope.shouldShowDeleteImage = !$scope.shouldShowDeleteImage;
+  }
+
+  $scope.cordovaCamera = function(options) {
+    var q = $q.defer();
+
+    navigator.camera.getPicture(function(result) {
+      q.resolve(result);
+    }, function(err) {
+      q.reject(err);
+    }, options);
+    return q.promise;
+  }
+
+  $scope.habilitaDelete = function() {
+    $scope.shouldShowDelete = !$scope.shouldShowDelete;
+  }
+
+  $scope.removeText = function(valor) {
+    $scope.vizualiza.splice(valor, 1);
+  }
+
+  $scope.removeFoto = function(valor) {
+    $scope.fotos.splice(valor, 1);
+  }
+
+  $scope.enviar = function(index) {
+
+    var listagemScope = Scopes.get('ListagemController')
+
+    if(!listagemScope.listaFotos) {
+      listagemScope.listaFotos = [];
+    }
+
+    listagemScope.listaFotos.push({palavras : $scope.vizualiza, fotos : $scope.fotos});
+    console.log("abacate auzl", listagemScope.vizualizaFotos)
+    $location.path("/listagem");
+
+  }
+
+
+  $scope.send = function() {
+    $ionicLoading.show({
+      template: 'OK, i got it'
+    })
+
+    setTimeout(function(){
+      $ionicLoading.hide().then(function() {
+      })
+    }, 3000);
+  }
+
+  $scope.testando = function() {
+    $ionicLoading.hide();
+  }
+
+  $scope.takePicture = function(){
+    var options = {
+      quality: 90,
+      correctOrientation : true,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      encodingType: Camera.EncodingType.JPEG,
+      allowEdit: true,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: false
+    };
+
+
+    $scope.cordovaCamera(options).then(function(imageData) {
+
+      $scope.fotos.push({"src" : "data:image/jpeg;base64," + imageData})
+      var imagem = new Image();
+      imagem.src = "data:image/jpeg;base64," + imageData;
+      var physicalScreenWidth = window.screen.width * window.devicePixelRatio;
+      var physicalScreenHeight = window.screen.height * window.devicePixelRatio;
+
+    }, function(err) {
+      console.log(err);
+    });
+  }
+
+
+  $scope.adicionaLinha = function() {
+    $scope.vizualiza.unshift({valor : ''});
+  };
+}]);
